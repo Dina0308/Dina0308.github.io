92bf627aeed8bb14bcd271d4a15c63a7dd832172
Dina <sharipova0308@gmail.com>
2022-01-22 18:28:50 +0500

    add form

--- js/script.js
+++ js/script.js
@@ -32,94 +32,7 @@ const swiper = new Swiper('.swiper-container', {
       prevEl: '.swiper-button-prev',
     }
 });
-
 //form
-// let forms = document.querySelectorAll('.form__elem');
-
-
-
-//   forms.forEach( form => {
-//     form.addEventListener('submit', (e) => {
-//       e.preventDefault();
-
-//       let formWrapper = form.closest('.form')
-//       let request = formWrapper.querySelector('.form__request');
-//       let answer = formWrapper.querySelector('.form__answer');
-
-
-
-
-//       let inputs = form.elements;
-//       let obj = {};
-
-//       for (let i = 0; i < inputs.length; i++) {
-
-
-
-//         let item = inputs.item(i)
-//         let name = item.name;
-//         let val = String(item.value).trim();
-//         let tag = item.tagName.toLowerCase();
-
-//         if (tag == 'button') {
-//           continue;
-//         }
-
-
-//         if (val == ""){
-//           item.style.border = "2px solid red";
-//           return false;
-//         } else {
-//           item.style.border = "none";
-//           obj[name] = val;
-//         } 
-
-//         console.log(obj);
-//       }
-
-//       //В переменную $token нужно вставить токен, который нам прислал @botFather
-//       const token = "5054421186:AAHzIR1yfMB6n519LDXQ5-9911ZbdqulnEA";
-//       //Сюда вставляем chat_id
-//       const chatId = "435805105";
-
-//       let text = '';
-//       for( let key in obj ) {
-//         text += `<b>${key}</b> ${obj[key]}\n`;
-//       }
-
-//       const parse_mode = "HTML";
-
-//       const requestOptionsPush = {
-//         method: 'POST',
-//         headers: {
-//           'Accept': 'application/json, text/plain, */*',
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(
-//           { chat_id: chatId, parse_mode: parse_mode, text: text }
-//         )
-//       };
-
-//       fetch(`https://api.telegram.org/bot${token}/sendMessage`, requestOptionsPush)
-//         .then(response => response.json())
-//         .then(response => {
-//           if (response.ok) {
-//             form.reset();
-//             request.style.display = "none"
-//             answer.style.display = "block"
-
-
-//           }
-//         });
-
-
-
-//     });
-//   });
-
-
-
-
 document.querySelectorAll(".form__elem").forEach(function(u) {
   u.addEventListener("submit", function(e) {
       e.preventDefault();
@@ -159,7 +72,11 @@ document.querySelectorAll(".form__elem").forEach(function(u) {
       })
   })
 })
-
+const modalClose = document.querySelector('.modal__close'),
+      modal = document.querySelector('.form__answer');
+modalClose.addEventListener('click', function() {
+      modal.hidden=true;
+});
 });
 
 
